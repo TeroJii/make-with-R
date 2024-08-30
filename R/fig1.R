329b11ce6732873a4f84ea28a1903dd3df70634e
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-08-30 11:39:11 +0300

    this creates a plot under R/Figs

--- R/fig1.R
+++ R/fig1.R
@@ -1,7 +1,9 @@
 # A script for creating a simple plot in R
 
-# load ggplot2 package, and if not installed, install it from CRAN
-if(!require(ggplot2)) install.packages("ggplot2")
+# add the library paths listed in .libPaths() to the search path
+#.libPaths(c("<lib-path1>", "<lib-path2>"))
+# load ggplot2 package
+library(ggplot2)
 
 my_plot <- mtcars |> 
   ggplot(aes(x = wt, y = mpg)) +
@@ -11,4 +13,9 @@ my_plot <- mtcars |>
        y = "Miles per Gallon") +
   theme_minimal()
 
-ggsave(filename = "../Figs/fig1.png", plot = my_plot, width = 6, height = 4, units = "in", dpi = 300)
+ggsave(filename = "Figs/fig1.pdf", plot = my_plot, device = "pdf",
+       width = 6, height = 4, units = "in", dpi = 300)
+
+# pdf("Figs/fig1.pdf", height=3, width=6.5, pointsize=12)
+# print(my_plot)
+# dev.off()
